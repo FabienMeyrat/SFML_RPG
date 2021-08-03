b986a8b30ddf1e24727c6d372b0d7a591f8e05f5
meyratf <fabien-meyrat@orange.fr>
2021-08-03 23:56:05 +0200

    State class

--- src/State.h
+++ src/State.h
@@ -5,8 +5,24 @@
 #ifndef SFML_RPG_STATE_H
 #define SFML_RPG_STATE_H
 
+#include <vector>
+
+#include "SFML/Graphics.hpp"
 
 class State {
+
+private:
+    // Variables
+    std::vector<sf::Texture> m_textures;
+
+public:
+    // Constructors - Destructors
+    State();
+    virtual ~State();
+
+    // Functions
+    virtual void update() = 0;
+    virtual void render() = 0 ;
 
 };
 
